5db4d3ae4901701e616a16cc1c985e7a58d209bc
RockoDev <contacto@rockodev.com>
2017-03-10 23:51:53 -0600

    Add support for older browsers

--- padStart.polyfill.js
+++ padStart.polyfill.js
@@ -5,7 +5,7 @@
     len = parseInt(len); if (isNaN(len)) len = 0;
     str = str === undefined ? '\u0020' : String(str);
     if (len <= this.length || str.length === 0) return String(this);
-    var buffer = new Array(len-this.length);
+    var buffer = new Array(len-this.length); str = str.split('');
     for (var i = 0; i < buffer.length; i++) buffer[i] = str[i%str.length];
     return buffer.join('').concat(this);
   };
