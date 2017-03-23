cf1cd98dbb48bb2f1ad8cb912771ac20aaca8bfd
RockoDev <contacto@rockodev.com>
2017-03-22 23:37:26 -0600

    Convert len parameter to integer using a bitwise operator

--- padStart.polyfill.js
+++ padStart.polyfill.js
@@ -2,8 +2,7 @@
   'use strict';
   if ('padStart' in String.prototype) return;
   String.prototype.padStart = function(len, str) {
-    len = parseInt(len); if (isNaN(len)) len = 0;
-    str = str === undefined ? '\u0020' : String(str);
+    len = len >> 0; str = str === undefined ? '\u0020' : String(str);
     if (len <= this.length || str.length === 0) return String(this);
     var buffer = new Array(len-this.length); str = str.split('');
     for (var i = 0; i < buffer.length; i++) buffer[i] = str[i%str.length];
